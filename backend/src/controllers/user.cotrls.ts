b1927e48a3d5f5bfed0545e49d7881951c3755f2
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2025-01-22 11:52:19 +0530

    user  module completes..

--- backend/src/controllers/user.cotrls.ts
+++ backend/src/controllers/user.cotrls.ts
@@ -1,6 +1,8 @@
 import { Request, Response} from "express";
 import User from "../models/user"
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs"
+
 
 
 
@@ -40,9 +42,61 @@ const Register=async(req:Request, res:Response): Promise<void> => {
 
 
 
-const Login  =async()=>{
+const Login  =async(req:Request, res:Response) : Promise<void> =>{
 
-}
+  const { email, password } = req.body;
+
+  try {
+    const user=await User.findOne({ email });
+    if(!user){
+      res.status(400).json({ message: "Invalid Credentials" });
+      return;
+    };
+
+    const isMatch= await bcrypt.compare(password, user.password);
+    if(!isMatch){
+      res.status(400).json({ message:"Invalid Credentials"});
+      return;
+    };
+
+    const token=jwt.sign(
+      {userId: user.id},
+      process.env.JWT_SECRET_KEY as string,
+      {expiresIn: "1d"},
+    );
+
+    res.cookie('auth_token',token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+   });
+
+   res.status(200).json({ userId: user._id });
+    
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+
+};
+
+const getMe=async(req:Request, res:Response) : Promise<void> => {
+
+  const userId=req.userId;
+
+  try {
+    const user= await User.findById(userId).select("-password");
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+      return;
+    };
+
+    res.json(user);
+    
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
+  }
+   
+};
 
 
 
@@ -52,4 +106,5 @@ const Login  =async()=>{
 export {
     Register,
     Login,
+    getMe,
 }
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,7 +1,8 @@
-import express from "express"
-import {Register} from "../controllers/user.cotrls"
+import express, {Request,Response} from "express"
+import {Login, Register,getMe} from "../controllers/user.cotrls"
 import { check} from "express-validator"
 import { validateRequest } from "../middlewares/validateMiddleware"
+import verifyToken from "../middlewares/auth"
 
 const router= express.Router()
 
@@ -15,6 +16,30 @@ router.post("/register",[
 validateRequest,
 Register
 );
+
+
+router.post("/login",[
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({ min: 6,}),
+ ],
+ validateRequest,
+ Login
+);
+
+
+router.get("/validate-token",verifyToken, (req:Request, res:Response)=> {
+    res.status(200).json({ userId: req.userId});
+});
+
+router.get("/me",verifyToken,getMe);
+
+router.post("/logout", (req:Request,res:Response)=>{
+    res.cookie("auth_token", "", {
+        expires: new Date(0),
+    });
+
+    res.send();
+});
 
 
 
