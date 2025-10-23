032b44514737a9c5e1e3eebc3541359a69caab27
salmanul faris pk <salmanulfarispk2001@gmail.com>
2025-10-23 16:01:07 +0530

    email issue

--- backend/src/helpers/PaymenyInvoice.ts
+++ backend/src/helpers/PaymenyInvoice.ts
@@ -92,9 +92,8 @@ export const createAndSendInvoice = async ({
   
       const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        service: 'gmail',
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASSWORD,
--- backend/src/helpers/cancelInvoice.ts
+++ backend/src/helpers/cancelInvoice.ts
@@ -94,9 +94,8 @@ export const CancellationInvoice = async ({
 
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            service: 'gmail',
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD,
@@ -158,7 +157,6 @@ export const CancellationInvoice = async ({
          const succesmail=await transporter.sendMail(mailOptions);
          console.log("all don esuccess",succesmail);
          
-        
         return updatedInvoice.id;
 
     } catch (error) {
